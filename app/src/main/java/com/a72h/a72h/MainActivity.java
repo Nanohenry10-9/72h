727e55ac52ab78f85ac5bb40100a9ed16ad61503
Nanohenry10-9 <irneha@hotmail.com>
2017-03-11 13:55:12 +0100

    Added onClick methods to buttons in mainActivity

--- app/src/main/java/com/a72h/a72h/MainActivity.java
+++ app/src/main/java/com/a72h/a72h/MainActivity.java
@@ -1,7 +1,9 @@
 package com.a72h.a72h;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void naturalDisasters(View view) {
+        Intent intent = new Intent(this, natural_disasters.class);
+        startActivity(intent);
+    }
+
+    public void checklist(View view) {
+        Intent intent = new Intent(this, checklist.class);
+        startActivity(intent);
+    }
+
+    public void meetingPoints(View view) {
+        //Intent intent = new Intent(this, checklist.class);
+        //startActivity(intent);
+    }
+
+    public void maps(View view) {
+        //Intent intent = new Intent(this, checklist.class);
+        //startActivity(intent);
+    }
+
+    public void powerOutage(View view) {
+        //Intent intent = new Intent(this, checklist.class);
+        //startActivity(intent);
+    }
+
+    public void firstAid(View view) {
+        //Intent intent = new Intent(this, checklist.class);
+        //startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/a72h/a72h/natural_disasters.java
@@ -0,0 +1,13 @@
+package com.a72h.a72h;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class natural_disasters extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_natural_disasters);
+    }
+}
