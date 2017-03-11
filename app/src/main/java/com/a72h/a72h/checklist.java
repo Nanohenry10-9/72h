492c054579389b9805e084477e73802b57236762
Nanohenry10-9 <irneha@hotmail.com>
2017-03-11 19:34:08 +0100

    Improved checklist menu

--- app/src/main/java/com/a72h/a72h/checklist.java
+++ app/src/main/java/com/a72h/a72h/checklist.java
@@ -1,9 +1,13 @@
 package com.a72h.a72h;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -17,14 +21,21 @@ public class checklist extends AppCompatActivity {
     }
 
     public void addItem(View view) {
-        EditText et = (EditText) findViewById(R.id.add1);
-        TextView tv = new TextView(view.getContext());
-        tv.setText(et.getText().toString());
+        EditText ed1 = new EditText(view.getContext());
+        EditText ed2 = new EditText(view.getContext());
+        EditText ed3 = new EditText(view.getContext());
+        ed1.setHint("Item Name");
+        ed2.setHint("Amount");
+        ed3.setHint("Exp. Date");
+        CheckBox cb = new CheckBox(view.getContext());
         TableLayout ll = (TableLayout) findViewById(R.id.tableView);
         TableRow row = new TableRow(view.getContext());
-        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 1);
+        row.addView(ed1);
+        row.addView(ed2);
+        row.addView(ed3);
+        row.addView(cb);
         row.setLayoutParams(lp);
-        row.addView(tv);
         ll.addView(row);
     }
 }
