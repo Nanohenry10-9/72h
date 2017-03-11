d79629d9c05cd7e244802060f6dcd22959731e19
Nanohenry10-9 <irneha@hotmail.com>
2017-03-11 16:13:45 +0100

    Improved checklist menu

--- app/src/main/java/com/a72h/a72h/checklist.java
+++ app/src/main/java/com/a72h/a72h/checklist.java
@@ -2,6 +2,11 @@ package com.a72h.a72h;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class checklist extends AppCompatActivity {
 
@@ -11,5 +16,15 @@ public class checklist extends AppCompatActivity {
         setContentView(R.layout.activity_checklist);
     }
 
-
+    public void addItem(View view) {
+        EditText et = (EditText) findViewById(R.id.add1);
+        TextView tv = new TextView(view.getContext());
+        tv.setText(et.getText().toString());
+        TableLayout ll = (TableLayout) findViewById(R.id.tableView);
+        TableRow row = new TableRow(view.getContext());
+        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+        row.setLayoutParams(lp);
+        row.addView(tv);
+        ll.addView(row);
+    }
 }
